0f50691872150ccf760ac2cf11f106adebe62172
TQ White II <tq@justkidding.com>
2017-12-18 16:59:38 -0600

    removed cruft, changed console.log to qtools.log..., ran prettier

--- api-manager.js
+++ api-manager.js
@@ -6,7 +6,6 @@ const async = require('async');
 //START OF moduleFunction() ============================================================
 
 var moduleFunction = function(args) {
-
 	qtools.validateProperties({
 		subject: args || {},
 		targetScope: this, //will add listed items to targetScope
@@ -21,7 +20,6 @@ var moduleFunction = function(args) {
 	this.apiStructure = {};
 	this.parentPath = '';
 
-
 	//LOCAL VARIABLES ====================================
 
 	let workerList = {};
@@ -30,27 +28,7 @@ var moduleFunction = function(args) {
 	//LOCAL FUNCTIONS ====================================
 
 	const startSystem = () => {
-
-		if (typeof (COMPONENT) == 'undefined') {
-			console.log("COMPONENT is undefined in" + __dirname)
-			this.initCallback && this.initCallback();
-			return;
-		}
-
-		const startList = [];
-
-		startList.push((done) => {
-			const workerName = 'COMPONENT_NAME'
-			new COMPONENT({
-				initCallback: function() {
-					workerList[workerName] = this; done();
-				}
-			});
-		});
-
-		async.series(startList, () => {
-			this.initCallback && this.initCallback();
-		});
+		this.initCallback && this.initCallback();
 	};
 
 	//METHODS AND PROPERTIES ====================================
@@ -58,17 +36,16 @@ var moduleFunction = function(args) {
 	this.registerApi = function(name, method) {
 		const parentPath = this.parentPath ? this.parentPath + '.' : '';
 		qtools.putSurePath(this.apiStructure, parentPath + name, method);
-	}
+	};
 
 	this.getApi = function(pathString) {
-
 		const apiFunction = qtools.getSurePath(this.apiStructure, pathString);
 		if (apiFunction) {
 			return qtools.getSurePath(this.apiStructure, pathString);
 		} else {
-			console.log(`apiManager.getApi() says, ${pathString} is not found`);
+			qtools.logError(`apiManager.getApi() says, ${pathString} is not found`);
 		}
-	}
+	};
 
 	this.init = (workerName, newRoot) => {
 		const parentPath = this.parentPath ? this.parentPath + '.' : '';
@@ -84,40 +61,50 @@ var moduleFunction = function(args) {
 		return descendent;
 	};
 
-	this.list = (label) => {
-		label = label ? '(at ' + label + ') ' : ''
-		const dump = qtools.dumpFlat({
-			"this.apiStructure": this.apiStructure
-		}, true).replace(/this\.apiStructure\./g, '');
-		console.log(`\n\napiManager method list ${label}=========================\n\n${dump}\nEND apiManager, =========================\n\n`)
-	}
+	this.list = label => {
+		label = label ? '(at ' + label + ') ' : '';
+		const dump = qtools
+			.dumpFlat(
+				{
+					'this.apiStructure': this.apiStructure
+				},
+				true
+			)
+			.replace(/this\.apiStructure\./g, '');
+		console.logDev(
+			`\n\napiManager method list ${label}=========================\n\n${dump}\nEND apiManager, =========================\n\n`
+		);
+	};
 
 	//API ENDPOINTS ====================================
 
 	//INITIALIZATION ====================================
 
-	console.log(__dirname);
 	startSystem();
-
 
 	//SHUTDOWN FUNCTIONS ====================================
 
-	if (typeof (workerList) != 'object' || 'this object DOES NOT WANT to SHUT DOWN WORKERS') {
+	if (
+		typeof workerList != 'object' ||
+		'this object DOES NOT WANT to SHUT DOWN WORKERS'
+	) {
 		this.shutdown = (message, callback) => {
-			console.log(`
-shutting down ${__dirname}`);
+			console.log(
+				`
+shutting down ${__dirname}`
+			);
 			callback('', message);
-		}
+		};
 	} else {
-		const buildShutdownList = (message) => {
+		const buildShutdownList = message => {
 			const shutdownList = [];
 			for (var i in workerList) {
 				var worker = workerList[i];
 				shutdownList.push(
-					((i) => {
-						return (done) => {
-							workerList[i].shutdown(message, done)
-						}
+					(i => {
+						return done => {
+							workerList[i].shutdown(message, done);
+						};
 					})(i)
 				);
 			}
@@ -130,16 +117,18 @@ shutting down ${__dirname}`);
 				workerList[i] = null;
 				nameString += `${i}, `;
 			}
-			qtools.message(`[${nameString.replace(/, $/, '')}] were flushed at ${Date.now()}`);
+			qtools.message(
+				`[${nameString.replace(/, $/, '')}] were flushed at ${Date.now()}`
+			);
 			workerList = {};
-		}
+		};
 
 		this.shutdown = (message, callback) => {
 			async.parallel(buildShutdownList(message), () => {
 				cleanup();
 				callback('', message);
 			});
-		}
+		};
 	}
 
 	return this;
@@ -149,4 +138,3 @@ shutting down ${__dirname}`);
 
 module.exports = moduleFunction;
 //module.exports = new moduleFunction();
-
